7456406ae82fa6cd5439899363c69298ec128bef
prmane03 <prmane03@gmail.com>
2021-03-30 11:19:57 +0530

    small changes

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask,request, url_for, redirect, render_template
 import tweepy
+import flask
 import time
 import datetime
 import pickle
@@ -30,6 +31,62 @@ api = tweepy.API(auth)
 
 app = Flask(__name__)
 
+CONSUMER_TOKEN='fG2WoInquUXqZB5bG2BWvVSeb'
+CONSUMER_SECRET='T2e6KSUoDs9g19FjpxKtRW0B9okz3SrAxYZd0vowymS6oNfkNv'
+CALLBACK_URL = 'http://thebotdetector.herokuapp.com/verify'
+session = dict()
+db = dict() #you can save these values to a database
+
+@app.route("/login")
+def send_token():
+    redirect_url= ''
+    auth = tweepy.OAuthHandler(str(CONSUMER_TOKEN), 
+        str(CONSUMER_SECRET), 
+        str(CALLBACK_URL))
+    try: 
+        #get the request tokens
+        redirect_url= auth.get_authorization_url()
+        print(redirect_url)
+        session['request_token']= (auth.request_token.key,
+            auth.request_token.secret)
+    except tweepy.TweepError:
+        print ('Error! Failed to get request token')
+    #this is twitter's url for authentication
+    return flask.redirect(redirect_url)	
+
+@app.route("/verify")
+def get_verification():
+
+	#get the verifier key from the request url
+	verifier= request.args['oauth_verifier']
+
+	auth = tweepy.OAuthHandler(CONSUMER_TOKEN, CONSUMER_SECRET)
+	token = session['request_token']
+	del session['request_token']
+
+	auth.set_request_token(token[0], token[1])
+
+	try:
+		    auth.get_access_token(verifier)
+	except tweepy.TweepError:
+		    print ('Error! Failed to get access token.')
+
+	#now you have access!
+	api = tweepy.API(auth)
+
+	#store in a db
+	db['api']=api
+	db['access_token_key']=auth.access_token.key
+	db['access_token_secret']=auth.access_token.secret
+	return flask.redirect(flask.url_for('start'))
+
+@app.route("/start")
+def start():
+	#auth done, app logic can begin
+	api = db['api']
+
+	#example, print your latest status posts
+	return flask.render_template('tweets.html', tweets=api.user_timeline())
 
 def GetData(scrnm):
             ipvect=[]
